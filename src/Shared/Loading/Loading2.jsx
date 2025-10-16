ea34c1185b030a1af9724418d04e78eea86a577e
003-demo <rrriku05@gmail.com>
2025-10-17 00:12:22 +0600

    loading2

--- src/Shared/Loading/Loading2.jsx
+++ src/Shared/Loading/Loading2.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Lottie from "lottie-react";
-import loading from "../../assets/loadingcircle.json";
+import loading from "../../assets/LoadingLine.json";
 
 const Loading2 = ({ size = 100 }) => {
   return (
-    <div className="flex items-center justify-center bg-info rounded-full shadow-2xl shadow-primary/50">
-      <Lottie 
-        animationData={loading} 
-        loop={true} 
-        autoplay 
-        className={`w-[${size}px] h-[${size}px]`} 
+    <div className="flex items-center justify-center rounded-full shadow-2xl shadow-primary/50">
+      <Lottie
+        animationData={loading}
+        loop={true}
+        autoplay
+        className={`w-[${size}px] h-[${size}px]`}
       />
     </div>
   );
